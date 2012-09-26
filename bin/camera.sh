03dddae2b61734da7717272b8858e8bd22a2d4f2
jefftherobot <jeffy9@gmail.com>
2012-09-26 16:20:03 -0400

    deleted some files

--- bin/camera.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cvlc v4l2:///dev/video0:width=320:height=240 --sout '#transcode{vcodec=mjpg,scale=1,fps=30,vb=4000}:standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8080}' --quiet --daemon &
